89c78c377ac4e0854f0eb52d8ee44fb3586f0103
DESKTOP-DELRI4U\kuros <mikisatoshi1992@gmail.com>
2018-10-15 22:05:31 +0900

    commit

--- readme.txt
+++ readme.txt
@@ -12,10 +12,18 @@
 
 ____________________________________________________
 
-TIPS
+crontab -e
+
+*/2 * * * * cd repo_loger && ./loger.sh
+30 1,13 * * * cd repo_loger && ./scheduler.sh
+00 2 * * * cd repo_loger && ./backup.sh
 
 crontab now runing
 
+--- TIPS ---
+
+git clone https://github.com/mikisatoshi/repo_loger.git
+git clone https://github.com/mikisatoshi/pi_loger.git
 
 cd pi_loger
 git pull origin master
---
+++ takephoto.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import json,datetime
+import picamera
+
+
+class PiLoger():
+  def __init__(self):
+    pass
+
+  def get_dummy_data(self):
+    pass
+
+def main():
+  with picamera.PiCamera() as camera:
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+    # Camera warm-up time
+    time.sleep(2)
+    camera.capture('my_picture.jpg')
+
+
+if __name__ == '__main__':
+  main()
