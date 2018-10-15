9b7bc5d55b55f9e64d59de00f6e4953537033eef
DESKTOP-DELRI4U\kuros <mikisatoshi1992@gmail.com>
2018-10-15 23:00:42 +0900

    commit

--- takephoto.py
+++ takephoto.py
@@ -11,13 +11,20 @@ class PiLoger():
     pass
 
 def main():
+  try:
+    with open("./../certification/unique_name.json") as f:
+      para = json.load(f)
+  except:
+    with open("./certification/unique_name.json") as f:
+      para = json.load(f)
+
   with picamera.PiCamera() as camera:
     camera.resolution = (1024, 768)
     camera.start_preview()
     # Camera warm-up time
     time.sleep(2)
     dt_now = datetime.datetime.now()
-    camera.capture('../storage/photo/'+str(datetime.datetime.today()) + '_' + str(dt_now.hour * 60 + dt_now.minute) + '.jpg')
+    camera.capture('../storage/photo/'+para['sheetname']+str(datetime.datetime.today()) + '_' + str(dt_now.hour * 60 + dt_now.minute) + '.jpg')
 
 
 if __name__ == '__main__':
