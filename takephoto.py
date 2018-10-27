e868a661069d6068decf244e64f2342a30774c57
DESKTOP-DELRI4U\kuros <mikisatoshi1992@gmail.com>
2018-10-27 15:42:11 +0900

    commit

--- takephoto.py
+++ takephoto.py
@@ -25,7 +25,7 @@ def main():
     time.sleep(2)
     dt_now = datetime.datetime.now()
     camera.capture('../storage/photo/'+para['sheetname']+'_'+str(datetime.date.today()) + '_' + str(dt_now.hour * 60 + dt_now.minute) + '.jpg')
-
+    print("took photo")
 
 if __name__ == '__main__':
   main()
